6714d5c709f534bd4d04f6b2d641b39761cbbd93
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-02-25 21:35:56 +0900

    Apply license

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # vivliostyle-util-insert-line-continuation-marker
 
+Add line continuation markers to elements.
+
 ![](./screenshot.png)
+
+## License
+
+This project is licensed under [GPL-3.0](LICENSE), except that everything in the `example` directory is released under CC0.
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // @ts-check
 
+/*
+ * vivliostyle-util-insert-line-continuation-marker - Add line continuation markers to elements.
+ * Copyright (C) 2025  Koutaro Mukai
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import fs from "node:fs";
 import { createCanvas } from "canvas";
 import { JSDOM } from "jsdom";
